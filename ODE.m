27388956c4328e3776a65003c30a542d447ac61a
Arian Velayati <78826553+avelayati@users.noreply.github.com>
2022-05-18 17:02:37 -0600

    Update ODE.m

--- ODE.m
+++ ODE.m
@@ -24,3 +24,26 @@ t = t0:h:tfinal; t = t';  % Store t in a variable
 plot(t,yout)
 xlabel("t")
 ylabel("y(t)")
+
+%% Example 3: Combustion model
+% dy/dt = y^2 - y^3; y(0) = y0; 0 <= t <= 2/y0
+
+F = @(t,y) y^2 - y^3; % F'
+
+t0 = 0 ; y0 = 0.01; tfinal = 2/y0; h = 0.3; % Initial condition, range, step size
+
+y = ODE4(F,t0,h,tfinal,y0); % Call solver
+
+
+% Live plot
+figure
+hold on
+for i=1:5*h:length(t)
+    plot(t(1:i),y(1:i),'.')
+    pause(0.05)
+    xlabel("t")
+    ylabel("y(t)")
+    xlim([0 200])
+    ylim([0 1])
+end
+
